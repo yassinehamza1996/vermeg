0f2e9ad10d5e2d0b2db3f080eda897c05591a7ee
yassinehamza1996 <yassine.hamza@etudiant-fst.utm.tn>
2020-10-25 22:14:22 +0100

    try

--- src/firstpack/Test.java
+++ src/firstpack/Test.java
@@ -6,6 +6,7 @@ public class Test {
 		// TODO Auto-generated method stub
 		System.out.println("Hello WORLD");
 		System.out.println("HELLO from master");
+		System.out.println("Hello from test");
 	}
 
 }
